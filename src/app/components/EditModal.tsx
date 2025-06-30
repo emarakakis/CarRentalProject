6e568edd887e1a0c7475500be8238e252a21bff0
emarakakis <eftihismarak@gmail.com>
2025-06-30 16:19:24 +0300

    Created the form for Editing a Car

--- src/app/components/EditModal.tsx
+++ src/app/components/EditModal.tsx
@@ -5,6 +5,10 @@ import DialogTitle from '@mui/material/DialogTitle'
 import DialogActions from '@mui/material/DialogActions'
 import Button from '@mui/material/Button'
 import { CarType } from './cars'
+import DialogContent from '@mui/material/DialogContent'
+import DialogContentText from '@mui/material/DialogContentText'
+import TextField from '@mui/material/TextField'
+import { useForm } from 'react-hook-form'
 
 type EditModal ={
     open: boolean
@@ -15,19 +19,36 @@ type EditModal ={
 export default function EditModal(props : EditModal){
     const {open, setOpen, car} = props
     const {id, name, brand, quantity, price: carPrice} = {...car}
+    const {register, handleSubmit, reset} = useForm<CarType>({
+        defaultValues:car
+    })
 
-    const handleClose = (reason:string) => {
+    const handleClose = () => {
+        setOpen(false)
+    }
+
+    const onSubmit = (data: CarType) => {
+        console.log(data)
         setOpen(false)
     }
 
     return (
-        <Dialog onClose={handleClose} open={open}>
-            <DialogTitle>Edit Car</DialogTitle>
+        <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>Edit Car</DialogTitle>
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <DialogContent>
+                <DialogContentText>Edit the current values and then save.</DialogContentText>
+                <TextField defaultValue={brand} {...register("brand")} fullWidth />
+                <TextField defaultValue={name} {...register("name")} fullWidth />
+                <TextField defaultValue={quantity} {...register("quantity")} fullWidth />
+                <TextField defaultValue={carPrice} {...register("price")} fullWidth />
+            </DialogContent>
             <DialogActions>
-                {name} {brand}
-                <Button>Ok</Button>
-                <Button>Cancel</Button>
+                <Button onClick={handleClose}>Cancel</Button>
+                <Button type="submit">Save</Button>
             </DialogActions>
+        </form>
         </Dialog>
+
     )
 }
