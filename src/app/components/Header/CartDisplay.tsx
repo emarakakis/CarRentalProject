9e474adac1ac385e53a460d09ef6ae0473174809
emarakakis <eftihismarak@gmail.com>
2025-07-01 10:12:12 +0300

    CartDisplay Provides the car names and quantities now

--- src/app/components/Header/CartDisplay.tsx
+++ src/app/components/Header/CartDisplay.tsx
@@ -4,6 +4,9 @@ import {Box, Drawer, Typography, Button, ListItem, ListItemText} from '@mui/mate
 import { CartContext } from '../Cart/cart-context'
 import { useContext } from 'react'
 import { CartType } from '../Cart/types'
+import { CarContext } from '../Cars/car-context'
+import { CarType } from '../CarItem/type'
+import { error } from 'console'
 
 type CartDisplayProps = {
   show: boolean
@@ -12,6 +15,7 @@ type CartDisplayProps = {
 
 export default function CartDisplay({ show, setShow }: CartDisplayProps) {
     const context = useContext(CartContext)
+    const {cars} = useContext(CarContext)
 
     if (!context){
         throw new Error("How")
@@ -23,14 +27,16 @@ export default function CartDisplay({ show, setShow }: CartDisplayProps) {
         setShow(false)
     }
 
+    const carInformation = findCarInformation(cars, cart.items)
+
     return (
         <Drawer anchor="right" open={show} onClose={handleClose}>
             <Box sx={{display: 'flex', flexDirection:"column", alignItems:"center", width: 300, padding: 2}}>
                 <Typography variant='h1'>Your Cart!</Typography>
-                {cart.items.map((c:{id: string, quantity: number}, i:number) => {
+                {carInformation.map((car: CarType & {quantity: number} , i:number) => {
                     return (
                         <ListItem key = {i}>
-                            <ListItemText>{c.id} {c.quantity}</ListItemText>
+                            <ListItemText>{car.brand} {car.name} of Quantity: {car.quantity}</ListItemText>
                         </ListItem>)
                 })}
                 <Button sx={{color:"black"}}>Buy</Button>
@@ -39,4 +45,16 @@ export default function CartDisplay({ show, setShow }: CartDisplayProps) {
             </Box>
         </Drawer>
     )
+}
+
+function findCarInformation(cars:CarType[], cartItems:{id: string, quantity: number}[]): CarType[]{
+    return cartItems.map(item => {
+        const car = cars.find(car => car.id === item.id)
+        if (!car){
+            throw new Error("Wtf")
+        } else {
+            return {...car, quantity: item.quantity}
+        }
+        
+    })
 }
